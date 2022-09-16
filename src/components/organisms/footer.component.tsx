14d85ce04dbf9a56078aba603a776fc3cfe26e61
Aldo Vincenti <personal@aldovincenti.com>
2022-09-17 00:11:14 +0200

    Update footer.component.tsx

--- src/components/organisms/footer.component.tsx
+++ src/components/organisms/footer.component.tsx
@@ -6,7 +6,7 @@ export const FooterComponent = () => {
   return (
     <div
       className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-12 xl:grid-cols-12 2xl:grid-cols-12 footer-anchor footer-container"
-      style={{ paddingTop: 40, paddingBottom: 40 }}
+      style={{ paddingTop: 40, paddingBottom: 40, paddingLeft: 20, paddingRight: 20  }}
     >
       <div className="col-span-3">
         <p className="text-lg">{translate('TEXT.COPYRIGHT')}</p>
